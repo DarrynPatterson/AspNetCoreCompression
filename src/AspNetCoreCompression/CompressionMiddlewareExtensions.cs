f8f97db751a1df1255d7aef5e71fc987a3f6457c
Darryn Patterson <darryn@whereismytransport.com>
2016-09-02 08:28:11 +0200

    Project initialization.

---
+++ src/AspNetCoreCompression/CompressionMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace AspNetCoreCompression
+{
+    public static class CompressionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCompression(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CompressionMiddleware>();
+        }
+    }
+}
